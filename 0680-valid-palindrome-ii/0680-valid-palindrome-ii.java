baf563a6b564845728faad11c322f8c4c9b50be4
NIKHIL GUPTA <152516486+nikhilgupta2533@users.noreply.github.com>
2025-06-25 12:18:12 +0530

    Time: 5 ms (22.99%), Space: 45.7 MB (25.74%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -3,23 +3,53 @@ class Solution {
         int i = 0;
         int j = s.length() - 1;
 
-        while (i < j) {
-            if (s.charAt(i) == s.charAt(j)) {
-                i++;
-                j--;
+        int count1 = 0;
+        int x = i, y = j;
+        while (x <= y) {
+            if (s.charAt(x) != s.charAt(y)) {
+                count1++;
+                x++;
+                // uske baad check karte raho
+                while (x <= y) {
+                    if (s.charAt(x) != s.charAt(y)) {
+                        count1 = 2; // more than 1 mismatch
+                        break;
+                    }
+                    x++;
+                    y--;
+                }
+                break;
             } else {
-                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+                x++;
+                y--;
             }
         }
 
-        return true;
-    }
-    private boolean isPalindrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
+        int count2 = 0;
+        i = 0;
+        j = s.length() - 1;
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                count2++;
+                j--;
+                while (i <= j) {
+                    if (s.charAt(i) != s.charAt(j)) {
+                        count2 = 2;
+                        break;
+                    }
+                    i++;
+                    j--;
+                }
+                break;
+            } else {
+                i++;
+                j--;
+            }
         }
-        return true;
+
+        if (count1 <= 1 || count2 <= 1) {
+            return true;
+        }
+        return false;
     }
 }
